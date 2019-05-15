7438702e9fd550ce9171f5ffea9412ac0d13125f
tunkhine126 <tunkhine126@gmail.com>
2019-05-15 16:59:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,39 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
-end
+
+  get '/articles/new' do 
+    erb :new
+  end 
+  
+  post '/articles' do 
+    @article = Article.find_or_create_by(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end 
+
+  get '/articles' do
+    erb :index 
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    erb :show
+  end 
+
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+    erb :edit
+  end 
+
+  patch '/articles/:id' do 
+    @article = Article.update(params[:id], title: params[:title], content: params[:content])
+    erb :show
+  end 
+
+  delete '/articles/:id' do 
+    @article = Article.destroy(params[:id])
+    redirect '/articles'
+
+  end  
+
+end 
